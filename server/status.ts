5e50413652c78080cf289b708e4822f4362cf943
Emma <emma2334@gmail.com>
2022-01-05 01:53:09 +0800

    feat(api): add status to response code

--- server/status.ts
+++ server/status.ts
@@ -19,36 +19,46 @@ interface Detail {
 export interface Result extends Detail {
   msg: string;
   code: number;
+  status: 'unknown' | 'successed' | 'failed';
 }
 
 /** Class representing status. */
 class Status {
   msg: string;
   code: number;
+  status: 'unknown' | 'successed' | 'failed';
 
   /**
    * Create status
    *
-   * @param      {string}  msg     The state information
-   * @param      {number}  code    The state code
+   * @param      {string}           msg        The state information
+   * @param      {number}           code       The state code
+   * @param      {0 | 1 | boolean}  successed  Is successed or not
    */
-  constructor (msg: string, code: number) {
+  constructor (msg: string, code: number, successed?: 0 | 1 | boolean) {
     this.msg = msg
     this.code = code
+    if (successed === 1 || successed === true) {
+      this.status = 'successed'
+    } else if (successed === 0 || successed === false) {
+      this.status = 'failed'
+    } else {
+      this.status = 'unknown'
+    }
   }
 }
 
 const SHORTEN_URL = {
-  CREATE_SUCCESS: new Status('建立縮網址成功', 1000),
-  CREATE_FAIL: new Status('建立縮網址失敗', 1001),
-  GET_METADATA_SUCCESS: new Status('獲取 Open Graph Metadata 成功', 1002),
-  GET_METADATA_FAIL: new Status('獲取 Open Graph Metadata 失敗', 1003),
-  FIND_SUCCESS: new Status('查找縮網址成功', 1004),
-  FIND_FAIL: new Status('查找縮網址失敗', 1005),
-  UPDATE_SUCCESS: new Status('更新縮網址內容成功', 1006),
-  UPDATE_FAIL: new Status('更新縮網址內容失敗', 1007),
-  UPDATE_VIEW_COUNT_SUCCESS: new Status('更新瀏覽次數成功', 1008),
-  UPDATE_VIEW_COUNT_FAIL: new Status('更新瀏覽次數失敗', 1009)
+  CREATE_SUCCESS: new Status('建立縮網址成功', 1000, true),
+  CREATE_FAIL: new Status('建立縮網址失敗', 1001, false),
+  GET_METADATA_SUCCESS: new Status('獲取 Open Graph Metadata 成功', 1002, true),
+  GET_METADATA_FAIL: new Status('獲取 Open Graph Metadata 失敗', 1003, false),
+  FIND_SUCCESS: new Status('查找縮網址成功', 1004, true),
+  FIND_FAIL: new Status('查找縮網址失敗', 1005, false),
+  UPDATE_SUCCESS: new Status('更新縮網址內容成功', 1006, true),
+  UPDATE_FAIL: new Status('更新縮網址內容失敗', 1007, false),
+  UPDATE_VIEW_COUNT_SUCCESS: new Status('更新瀏覽次數成功', 1008, true),
+  UPDATE_VIEW_COUNT_FAIL: new Status('更新瀏覽次數失敗', 1009, false)
 }
 
 export const code = { SHORTEN_URL }
