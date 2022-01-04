e246fd20626ed909d1ee49eaff4f79ff9c828805
Emma <emma2334@gmail.com>
2022-01-05 04:10:53 +0800

    feat(client): creat component UrlInput

---
+++ client/src/components/UrlInput/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import cx from 'classnames'
+import { Form, Input, Button } from 'antd'
+
+import style from './index.module.css'
+
+interface PropsType {
+  onFinish: (values: any) => void;
+  buttonDisabled?: boolean;
+}
+
+const UrlInput = ({ onFinish, buttonDisabled }: PropsType) => {
+  return (
+    <Form className={style.form} layout="inline" onFinish={onFinish}>
+      <Form.Item
+        className={cx(style.input, style.formItem)}
+        name="url"
+        rules={[
+          {
+            required: true,
+            pattern:
+              /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi,
+            message: '請確保網址格式正確，例如：https://github.com/'
+          }
+        ]}
+      >
+        <Input placeholder="網址" />
+      </Form.Item>
+      <Form.Item className={style.formItem}>
+        <Button type="primary" htmlType="submit" disabled={buttonDisabled}>
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
+
+export default React.memo(UrlInput)
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
-import { Form, Input, Button } from 'antd'
 import { post } from 'utils/api'
 import ResponseAlert from 'components/ResponseAlert'
+import UrlInput from 'components/UrlInput'
 import UrlInfo from 'components/UrlInfo'
 
 const Home = () => {
   const [urlInfo, setUrlInfo] = useState<any>({})
   const [loading, setLoading] = useState(false)
-  const onFinish = async ({ url }: { url: string }) => {
+  const onFinish = ({ url }: { url: string }) => {
     setLoading(true)
     post('/api/url', { url }).then((res) => {
       setLoading(false)
@@ -18,26 +18,7 @@ const Home = () => {
 
   return (
     <>
-      <Form layout="inline" onFinish={onFinish}>
-        <Form.Item
-          name="url"
-          rules={[
-            {
-              required: true,
-              pattern:
-                /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi,
-              message: '請確保網址格式正確，例如：https://github.com/'
-            }
-          ]}
-        >
-          <Input placeholder="網址" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" disabled={loading}>
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+      <UrlInput onFinish={onFinish} buttonDisabled={loading} />
       {!loading && urlInfo.code && <ResponseAlert {...urlInfo} />}
       {(loading || urlInfo.data) && (
         <UrlInfo loading={loading} data={urlInfo.data} />
