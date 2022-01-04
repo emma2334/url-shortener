b7bd056a85200971af2c81fb94b6e4fdb7df283f
Emma <emma2334@gmail.com>
2022-01-04 19:58:33 +0800

    chore(server): add cors

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,14 @@
 import express from 'express'
-import bodyParser from 'body-parser'
+import cors from 'cors'
 import URL from './features/url'
 
 const app = express()
 const port = process.env.PORT || 3001
 
 app.set('view engine', 'pug')
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
