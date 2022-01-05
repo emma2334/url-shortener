c0604bc36e31c0b3f25f918690799c21d0f9fa90
Emma <emma2334@gmail.com>
2022-01-06 02:07:32 +0800

    feat: serve static file from react on server

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import path from 'path'
 import URL from './features/url'
 
 const app = express()
@@ -9,6 +10,7 @@ app.set('view engine', 'pug')
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '../client/build')))
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
@@ -36,4 +38,8 @@ app.route('/:id').get(async (req, res) => {
       favicon: target.data.favicon
     })
     : res.status(404).render('404')
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
 })
